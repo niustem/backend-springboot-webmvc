436f3a2f340cba86107a083a8afd1ee3d366e9c8
alex <271617800@qq.com>
2020-12-23 22:09:30 +0800

    create from spring initializr

---
+++ src/main/java/com/niustem/backend/springboot/BackendSpringbootWebmvcApplication.java
@@ -0,0 +1,13 @@
+package com.niustem.backend.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringbootWebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringbootWebmvcApplication.class, args);
+	}
+
+}
